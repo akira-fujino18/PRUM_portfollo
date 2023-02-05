a866b0ae826fb7ae6407f7c74f544f9e4bfd4959
Ubuntu <ubuntu@ip-172-31-88-254.ec2.internal>
2023-02-05 06:10:12 +0000

    fixed finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_31_225551) do
+ActiveRecord::Schema.define(version: 2023_01_31_224143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "self_introduciton"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
